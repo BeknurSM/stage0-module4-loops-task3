ba5e59f9eb6eae393974510424bd4d142ca6f6cc
BekoSm <b.smagulov.1999@gmail.com>
2022-07-22 15:47:15 +0600

    Push#1

--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
-
+        int i,t=9,last=0;
+        int sum = 0;
+        for (i=1;i<=lengthOfLastNumber;i++)
+        {
+            sum +=t;
+            last = last + t;
+            t=t*10+9;
+        }
+        System.out.println(last);
     }
 }
